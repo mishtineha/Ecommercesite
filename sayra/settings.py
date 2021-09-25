c3c72212f3a2f64cf5e519ac5ea71ac8451c442c
Neha <neha.mishra@industrybuying.com>
2021-09-25 20:59:55 +0530

    static root in settings.py file

--- sayra/settings.py
+++ sayra/settings.py
@@ -232,7 +232,7 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-  # os.path.join(BASE_DIR, "static"),
-  '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#   # os.path.join(BASE_DIR, "static"),
+#   '/var/www/static/',
+# ]
