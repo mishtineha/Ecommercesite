20788148d8756a9266311391ed8fce2ad20f0db1
Neha <neha.mishra@industrybuying.com>
2021-09-26 10:39:30 +0530

    static root and media root change to public_html folder

--- sayra/settings.py
+++ sayra/settings.py
@@ -224,14 +224,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/sayrain/public_html/media/'
 MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/sayrain/public_html/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #   # os.path.join(BASE_DIR, "static"),
 #   '/var/www/static/',
